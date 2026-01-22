05b5851d43fe754c9338275dfa809cdf67e33309
Abhinav <abhinav.k@gnapi.tech>
2026-01-22 16:04:56 +0530

    package.json changed according to shifted from js to ts

--- book_backend/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://127.0.0.1:27017/mydatabase")
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.error(err));
-
-module.exports = mongoose;
--- book_backend/src/middleware/authMiddleware.ts
+++ book_backend/src/middleware/authMiddleware.ts
@@ -1,4 +1,5 @@
-import { Request, Response, NextFunction } from "express";import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
 dotenv.config();
